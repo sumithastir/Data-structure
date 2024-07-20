12aa949e3ed79a7d3f49523f29fe0c70c95d6b7d
Amit Hastir <amithastir@gmail.com>
2024-07-21 00:22:24 +0530

    interview problem Problems statement file

---
+++ day16-length-of-longest-consecutive-ones.py
@@ -0,0 +1,32 @@
+'''
+Problems Statement:
+Given a binary string A. It is allowed to do at most one swap between any 0 and 1.
+Find and return the length of the longest consecutive 1’s that can be achieved.
+
+Problem Constraints:
+1 <= length of string <= 1000000
+A contains only characters 0 and 1.
+
+Input Format:
+The only argument given is string A.
+
+Output Format:
+Return the length of the longest consecutive 1’s that can be achieved.
+
+Example input:
+Input 1:
+    A = "111000"
+Output 1:
+    3
+
+Input 2:
+    A = "111011101"
+Output 2:
+    7
+
+'''
+
+class Solution:
+    def solve(self, A):
+        pass
+
